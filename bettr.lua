f77c4be44aa6d47aed7857c849e2875f89f73ecd
Fan of fish <81502024+WhyGitHubb@users.noreply.github.com>
2022-03-07 17:57:36 +0800

    Update bettr.lua

--- bettr.lua
+++ bettr.lua
@@ -10,160 +10,204 @@ setscriptable(game:GetService("Lighting"), "EnvironmentDiffuseScale", true)
 setscriptable(game:GetService("Lighting"), "EnvironmentSpecularScale", true)
 setscriptable(game:GetService("Lighting"), "GlobalShadows", true)
 
+setscriptable(game:GetService("Workspace"), "LevelOfDetail", true)
+game:GetService("Workspace").LevelOfDetail = Enum.ModelLevelOfDetail.Disabled
+
+settings().Rendering.QualityLevel = "Level01"
+game:GetService("Lighting").GlobalShadows = false
+game:GetService("Lighting").FogEnd = 9e9
+game:GetService("Lighting").Brightness = 0
+game:GetService("Workspace").Terrain.WaterWaveSize = 0
+game:GetService("Workspace").Terrain.WaterWaveSpeed = 0
+game:GetService("Workspace").Terrain.WaterReflectance = 0
+game:GetService("Workspace").Terrain.WaterTransparency = 0
+
+ for _,v in pairs(game:GetService("Lighting"):GetChildren()) do 
+        if v:IsA("BlurEffect") or v:IsA("BloomEffect") or v:IsA("SunRaysEffect") and v.Name ~= "" then 
+            v.Enabled = false 
+        end
+    end
+
+ for i,e in pairs(game:GetService("Lighting"):GetChildren()) do
+        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+            e.Enabled = false
+        end
+    end
 
 local table_insert
 do -- Globals
-table_insert = table.insert
+    table_insert = table.insert
 end
 local _gethiddenproperty
 local _sethiddenproperty
 do -- Implementations
-_gethiddenproperty = gethiddenproperty or gethiddenprop or get_hidden_property or get_hidden_prop or getpropvalue or function(_Instance, _string, ...)
-local v_boolean_indexSucceeded, v_any_propValue = pcall(function()
-return _Instance[_string]
-end)
-return v_boolean_indexSucceeded and v_any_propValue
-end
-_sethiddenproperty = sethiddenproperty or sethiddenprop or set_hidden_property or set_hidden_prop or setpropvalue or function(_Instance, _string, _any, ...)
-local v_boolean_newIndexSucceeded, v_string_error = pcall(function()
-_Instance[_string] = _any
-end)
-if not v_boolean_newIndexSucceeded then
-local v_number_stackLevel = 2
-error(v_string_error, v_number_stackLevel)
-end
-end
+    _gethiddenproperty =
+        gethiddenproperty or gethiddenprop or get_hidden_property or get_hidden_prop or getpropvalue or
+        function(_Instance, _string, ...)
+            local v_boolean_indexSucceeded, v_any_propValue =
+                pcall(
+                function()
+                    return _Instance[_string]
+                end
+            )
+            return v_boolean_indexSucceeded and v_any_propValue
+        end
+    _sethiddenproperty =
+        sethiddenproperty or sethiddenprop or set_hidden_property or set_hidden_prop or setpropvalue or
+        function(_Instance, _string, _any, ...)
+            local v_boolean_newIndexSucceeded, v_string_error =
+                pcall(
+                function()
+                    _Instance[_string] = _any
+                end
+            )
+            if not v_boolean_newIndexSucceeded then
+                local v_number_stackLevel = 2
+                error(v_string_error, v_number_stackLevel)
+            end
+        end
 end
 local GetService
 local GetDescendants
 local IsA
 do -- Methods
-GetService = game.GetService
-GetDescendants = game.GetDescendants
-IsA = game.IsA
+    GetService = game.GetService
+    GetDescendants = game.GetDescendants
+    IsA = game.IsA
 end
 local Lighting
 local NetworkSettings, RenderSettings, GameSettings
 local UserGameSettings
 do -- Services
-Lighting = GetService(game, "Lighting")
-NetworkSettings, RenderSettings, GameSettings = (function(...)
-local v_Instance_globalSettings = settings(...)
-return GetService(v_Instance_globalSettings, "NetworkSettings"), GetService(v_Instance_globalSettings, "RenderSettings"), GetService(v_Instance_globalSettings, "GameSettings")
-end)()
-UserGameSettings = GetService(UserSettings(...), "UserGameSettings")
+    Lighting = GetService(game, "Lighting")
+    NetworkSettings, RenderSettings, GameSettings =
+        (function(...)
+        local v_Instance_globalSettings = settings(...)
+        return GetService(v_Instance_globalSettings, "NetworkSettings"), GetService(
+            v_Instance_globalSettings,
+            "RenderSettings"
+        ), GetService(v_Instance_globalSettings, "GameSettings")
+    end)()
+    UserGameSettings = GetService(UserSettings(...), "UserGameSettings")
 end
 local Properties
 do -- Locals
-Properties = {
-QualityLevel = {
-RenderSettings,
-Enum.QualityLevel.Level01
-},
-MeshPartDetailLevel = {
-RenderSettings,
-Enum.MeshPartDetailLevel.Level01
-},
-EagerBulkExecution = {
-RenderSettings,
-false
-}, -- Rendering budget limit
-SavedQualityLevel = {
-UserGameSettings,
-Enum.SavedQualitySetting.QualityLevel1
-},
-Technology = {
-Lighting,
-Enum.Technology.Legacy
-},
-EnvironmentDiffuseScale = {
-Lighting,
-0
-},
-EnvironmentSpecularScale = {
-Lighting,
-0
-},
-PhysicsSteppingMethod = {
-workspace,
-Enum.PhysicsSteppingMethod.Default
-},
-SignalBehavior = {
-workspace,
-Enum.SignalBehavior.Deferred
-},
-MeshPartHeadsAndAccessories = {
-workspace,
-Enum.MeshPartHeadsAndAccessories.Enabled
-},
-ClientAnimatorThrottling = {
-workspace,
-Enum.ClientAnimatorThrottlingMode.Enabled
-},
-InterpolationThrottling = {
-workspace,
-Enum.InterpolationThrottlingMode.Enabled
-},
-GlobalShadows = {
-Lighting,
-false
-}, -- Shadows on
-Decoration = {
-workspace.Terrain,
-false
-}, -- Grass on
-WaterTransparency = {
-workspace.Terrain,
-0
-}, -- Transparent water
-IncomingReplicationLag = {
-NetworkSettings,
--1000
-}, -- lagn't
-HasEverUsedVR = {
-UserGameSettings,
-true
-} -- funni
-}
+    Properties = {
+        QualityLevel = {
+            RenderSettings,
+            Enum.QualityLevel.Level01
+        },
+        MeshPartDetailLevel = {
+            RenderSettings,
+            Enum.MeshPartDetailLevel.Level01
+        },
+        EagerBulkExecution = {
+            RenderSettings,
+            false
+        }, -- Rendering budget limit
+        SavedQualityLevel = {
+            UserGameSettings,
+            Enum.SavedQualitySetting.QualityLevel1
+        },
+        Technology = {
+            Lighting,
+            Enum.Technology.Legacy
+        },
+        EnvironmentDiffuseScale = {
+            Lighting,
+            0
+        },
+        EnvironmentSpecularScale = {
+            Lighting,
+            0
+        },
+        PhysicsSteppingMethod = {
+            workspace,
+            Enum.PhysicsSteppingMethod.Default
+        },
+        SignalBehavior = {
+            workspace,
+            Enum.SignalBehavior.Deferred
+        },
+        MeshPartHeadsAndAccessories = {
+            workspace,
+            Enum.MeshPartHeadsAndAccessories.Enabled
+        },
+        ClientAnimatorThrottling = {
+            workspace,
+            Enum.ClientAnimatorThrottlingMode.Enabled
+        },
+        InterpolationThrottling = {
+            workspace,
+            Enum.InterpolationThrottlingMode.Enabled
+        },
+        GlobalShadows = {
+            Lighting,
+            false
+        }, -- Shadows on
+        Decoration = {
+            workspace.Terrain,
+            false
+        }, -- Grass on
+        WaterTransparency = {
+            workspace.Terrain,
+            0
+        }, -- Transparent water
+        IncomingReplicationLag = {
+            NetworkSettings,
+            -1000
+        }, -- lagn't
+        HasEverUsedVR = {
+            UserGameSettings,
+            true
+        } -- funni
+    }
 end
 local EnumItem_ModelLevelOfDetail_Disabled
 do -- EnumItems
-EnumItem_ModelLevelOfDetail_Disabled = Enum.ModelLevelOfDetail.Disabled
+    EnumItem_ModelLevelOfDetail_Disabled = Enum.ModelLevelOfDetail.Disabled
 end
 do -- Source
-do -- Change Properties
-for v_string_propName, v_table_propData in next, Properties do
-local v_Instance_propHolder = v_table_propData[1]
-local v_any_propValue = v_table_propData[2]
-_sethiddenproperty(v_Instance_propHolder, v_string_propName, v_any_propValue)
-end
-end
-do -- MeshParts with their DoubleSided property
-local v_table_meshParts = (function(...)
-local v_table_meshParts = {}
-for v_number_index, v_Instance_instance in ipairs(GetDescendants(game)) do
-if IsA(v_Instance_instance, "MeshPart") and _gethiddenproperty(v_Instance_instance, "DoubleSided") == true then
-table_insert(v_table_meshParts, v_Instance_instance)
-end
-end
-return v_table_meshParts
-end)()
-for v_number_index, v_MeshPart_instance in ipairs(v_table_meshParts) do
-_sethiddenproperty(v_MeshPart_instance, "DoubleSided", false)
-end
-end
-do -- Models with their LevelOfDetail property
-local v_table_models = (function(...)
-local v_table_models = {}
-for v_number_index, v_Instance_instance in ipairs(GetDescendants(game)) do
-if IsA(v_Instance_instance, "Model") and _gethiddenproperty(v_Instance_instance, "LevelOfDetail") ~= EnumItem_ModelLevelOfDetail_Disabled then
-table_insert(v_table_models, v_Instance_instance)
-end
-end
-return v_table_models
-end)()
-for v_number_index, v_Model_instance in ipairs(v_table_models) do
-_sethiddenproperty(v_Model_instance, "LevelOfDetail", EnumItem_ModelLevelOfDetail_Disabled)
-end
-end
+    do -- Change Properties
+        for v_string_propName, v_table_propData in next, Properties do
+            local v_Instance_propHolder = v_table_propData[1]
+            local v_any_propValue = v_table_propData[2]
+            _sethiddenproperty(v_Instance_propHolder, v_string_propName, v_any_propValue)
+        end
+    end
+    do -- MeshParts with their DoubleSided property
+        local v_table_meshParts = (function(...)
+            local v_table_meshParts = {}
+            for v_number_index, v_Instance_instance in ipairs(GetDescendants(game)) do
+                if
+                    IsA(v_Instance_instance, "MeshPart") and
+                        _gethiddenproperty(v_Instance_instance, "DoubleSided") == true
+                 then
+                    table_insert(v_table_meshParts, v_Instance_instance)
+                end
+            end
+            return v_table_meshParts
+        end)()
+        for v_number_index, v_MeshPart_instance in ipairs(v_table_meshParts) do
+            _sethiddenproperty(v_MeshPart_instance, "DoubleSided", false)
+        end
+    end
+    do -- Models with their LevelOfDetail property
+        local v_table_models =
+            (function(...)
+            local v_table_models = {}
+            for v_number_index, v_Instance_instance in ipairs(GetDescendants(game)) do
+                if
+                    IsA(v_Instance_instance, "Model") and
+                        _gethiddenproperty(v_Instance_instance, "LevelOfDetail") ~= EnumItem_ModelLevelOfDetail_Disabled
+                 then
+                    table_insert(v_table_models, v_Instance_instance)
+                end
+            end
+            return v_table_models
+        end)()
+        for v_number_index, v_Model_instance in ipairs(v_table_models) do
+            _sethiddenproperty(v_Model_instance, "LevelOfDetail", EnumItem_ModelLevelOfDetail_Disabled)
+        end
+    end
 end
